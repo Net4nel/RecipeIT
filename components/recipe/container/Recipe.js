947c1d17f9d4f805fdf08b400166e4ae0155ade1
Net4nel <Net4nel@gmail.com>
2017-04-27 22:32:49 +0300

    Form New Recipe had been updated.

--- components/recipe/container/Recipe.js
+++ components/recipe/container/Recipe.js
@@ -5,35 +5,78 @@ export default class Recipe extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            title: '',
+            tempIngredient: '',
+            tempStep: '',
+            tempKeyword: '',
             steps: [],
             ingredients: [],
             keyWords: []
+        };
+
+        this.onChange = this.onChange.bind(this);
+        this.addKeyWord = this.addKeyWord.bind(this);
+        this.addIngredients = this.addIngredients.bind(this);
+        this.addStep = this.addStep.bind(this);
+    }
+
+    onChange(e) {
+        debugger;
+        let input = e.target;
+        let name = input.getAttribute('name');
+        switch (name) {
+            case 'title':
+                this.setState({
+                    title: input.value
+                });
+                break;
+            case 'tempIngredient':
+                this.setState({
+                    tempIngredient: input.value
+                });
+                break;
+            case 'tempStep':
+                this.setState({
+                    tempStep: input.value
+                });
+                break;
+            case 'tempKeyWord':
+                this.setState({
+                    tempKeyword: input.value
+                });
+                break;
         }
     }
 
-    addKeyWord(keyword) {
+    addKeyWord() {
         let keywords = this.state.keyWords;
+        let keyword = this.state.tempKeyword;
         if (!~keywords.indexOf(keyword)) { // ~ indexOf returns true if exists false if not.
             this.setState({
-                keyWords: [...this.state.keyWords, keyword]
+                keyWords: [...this.state.keyWords, keyword],
+                tempKeyword: ''
             })
         }
     }
 
-    addIngredients(ingredient) {
+    addIngredients() {
         let ingredients = this.state.ingredients;
+        let ingredient = this.state.tempIngredient;
         if (!~ingredients.indexOf(ingredient)) { // ~ indexOf returns true if exists false if not.
             this.setState({
-                ingredients: [...this.state.ingredients, ingredient]
+                ingredients: [...this.state.ingredients, ingredient],
+                tempIngredient: ''
             })
         }
     }
 
-    addStep(step) {
+    addStep() {
         let steps = this.state.steps;
+        let step = this.state.tempStep;
         if (!~steps.indexOf(step)) { // ~ indexOf returns true if exists false if not.
             this.setState({
-                steps: [...this.state.steps, step]
+                steps: [...this.state.steps, step],
+                tempStep: ''
             })
         }
     }
@@ -43,7 +86,19 @@ export default class Recipe extends Component {
         let { steps, ingredients, keyWords} = this.state;
         return (
             <div className="recipe-wrapper container">
-                <Form steps={steps} ingredients={ingredients} keyWords={keyWords}/>
+                <Form
+                    steps={steps}
+                    ingredients={ingredients}
+                    keyWords={keyWords}
+                    tempStep={this.state.tempStep}
+                    tempIngredient={this.state.tempIngredient}
+                    tempKeyWord={this.state.tempKeyword}
+                    addKeyWord={this.addKeyWord}
+                    addIngredient={this.addIngredients}
+                    addStep={this.addStep}
+                    onChange={this.onChange}
+                    title={this.state.title}
+                />
             </div>
         );
     }
--- components/recipe/display/RecipeForm.js
+++ components/recipe/display/RecipeForm.js
@@ -12,7 +12,7 @@ const RecipeForm = (props) => {
                             <label>שם המתכון</label>
                         </div>
                         <div className="col-md-10">
-                            <input type="text" className="form-control" />
+                            <input name="title" value={props.title} onChange={props.onChange} type="text" className="form-control" />
                         </div>
                     </div>
                     <div className="input-wrapper row">
@@ -20,10 +20,10 @@ const RecipeForm = (props) => {
                             <label>הזן מילות מפתח</label>
                         </div>
                         <div className="col-md-9">
-                            <input type="text" className="form-control" />
+                            <input name="tempKeyWord" value={props.tempKeyWord} onChange={props.onChange}  type="text" className="form-control" />
                         </div>
                         <div className="col-md-1">
-                            <input type="button" value="+" />
+                            <input type="button" value="+" onClick={props.addKeyWord}/>
                         </div>
                     </div>
                     <div className="key-words-wrapper row">
@@ -35,8 +35,8 @@ const RecipeForm = (props) => {
                     </div>
                     <div className="input-wrapper row">
                         <label>הזן מצרכים</label>
-                        <input type="text" className="form-control" />
-                        <input type="button" value="+" />
+                        <input name="tempIngredient" value={props.tempIngredient} onChange={props.onChange}  type="text" className="form-control" />
+                        <input type="button" value="+" onClick={props.addIngredient}/>
                     </div>
                     <div className="ingredients-wrapper row">
                         <ul className="ingredients-list">
@@ -47,8 +47,8 @@ const RecipeForm = (props) => {
                     </div>
                     <div className="input-wrapper row">
                         <label>הזן את שלבי המתכון</label>
-                        <input type="text" className="form-control" />
-                        <input type="button" value="+" />
+                        <input name="tempStep" value={props.tempStep} onChange={props.onChange}  type="text" className="form-control" />
+                        <input type="button" value="+" onClick={props.addStep} />
                     </div>
                     <div className="steps-wrapper row">
                         <ol className="steps-list">
@@ -72,8 +72,12 @@ RecipeForm.propTypes = {
     keyWords: PropTypes.Array,
     addKeyWord: PropTypes.Function,
     addIngredient: PropTypes.Function,
-    addStep: PropTypes.Function
-
+    addStep: PropTypes.Function,
+    onChange: PropTypes.Function,
+    tempStep: PropTypes.String,
+    tempKeyWord: PropTypes.String,
+    tempIngredient: PropTypes.String,
+    title: PropTypes.String
 };
 
 export default RecipeForm;
